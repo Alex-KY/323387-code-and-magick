bda17250fc998bf94e5905c3546e0cf3fd2217a3
Alex_KY <kravchenko_ay@mail.ru>
2018-02-03 19:10:05 +0300

    Улучшения по предложению наставника
    1. Добавлена функция выбора случайного цвета
    2. Добавлена функция выбора случайной прозрачности, исключая 0.
    3. Некоторые значения записаны в переменные для лучшей читаемости кода.
    4. Добавлена функция отрисовки строк.

--- js/stat.js
+++ js/stat.js
@@ -9,6 +9,7 @@ var FONT_GAP = 15;
 var BAR_GAP = 50;
 var BAR_WIDTH = 40;
 var barHeight = 150;
+var stringText = 1;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -27,6 +28,21 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var drawStringText = function (ctx, text) {
+  var string = CLOUD_Y + GAP + (FONT_GAP + GAP / 2) * stringText;
+  stringText++;
+  ctx.fillText(text, CLOUD_X + FONT_GAP, string);
+};
+
+// Функция для выбора случайного цвета палитры rgb
+// var getRandomColor = function () {
+//   return (255 * (Math.random())).toFixed(0) + ', ' + (255 * (Math.random())).toFixed(0) + ', ' + (255 * (Math.random())).toFixed(0);
+// };
+
+var getRandomTransparent = function () {
+  return (Math.floor(Math.random() * 10) + 1) / 10;
+};
+
 window.renderStatistics = function (ctx, players, times) {
 
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
@@ -36,20 +52,23 @@ window.renderStatistics = function (ctx, players, times) {
   ctx.strokeRect(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
 
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', CLOUD_X + FONT_GAP, CLOUD_Y + FONT_GAP + FONT_GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + FONT_GAP, CLOUD_Y + (FONT_GAP + GAP) * 2);
+  drawStringText(ctx, 'Ура вы победили!');
+  drawStringText(ctx, 'Список результатов:');
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
+    var playerBarHeight = (barHeight * times[i]) / maxTime;
+    var playerBarIndent = CLOUD_X + FONT_GAP + (BAR_WIDTH + BAR_GAP) * i;
 
-    ctx.fillText(players[i], CLOUD_X + FONT_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT + CLOUD_Y - FONT_GAP);
+    ctx.fillText(players[i], playerBarIndent, CLOUD_HEIGHT + CLOUD_Y - FONT_GAP);
 
-    ctx.fillStyle = (players[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + Math.random().toFixed(1) + ')';
+    ctx.fillStyle = (players[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + getRandomTransparent() + ')';
 
-    ctx.fillRect(CLOUD_X + FONT_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT + GAP - FONT_GAP - FONT_GAP - (barHeight * times[i]) / maxTime, BAR_WIDTH, (barHeight * times[i]) / maxTime);
+    ctx.fillRect(playerBarIndent, CLOUD_HEIGHT + GAP - 2 * FONT_GAP - playerBarHeight, BAR_WIDTH, playerBarHeight);
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), CLOUD_X + FONT_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - FONT_GAP + FONT_GAP - FONT_GAP - GAP - (barHeight * times[i]) / maxTime);
+    ctx.fillText(Math.round(times[i]), playerBarIndent, CLOUD_HEIGHT - FONT_GAP - GAP - playerBarHeight);
 
   }
+  stringText = 1;
 };
